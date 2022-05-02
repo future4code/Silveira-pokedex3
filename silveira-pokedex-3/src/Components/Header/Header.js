13701a3eac034b2cf2139f437066beba9b984545
Adeir Maia <adeir.maia@hotmail.com>
2022-05-02 17:56:36 -0300

    Add Header Pokeball

---
+++ silveira-pokedex-3/src/Components/Header/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { ContainerHeader, Red, Black, White, SuportPokeball } from './styles'
+import { ComponetPokeball } from '../Pokeball/Pokeball'
+
+const Header = () => {
+  return (
+    <ContainerHeader>
+      <Red></Red>
+      <Black></Black>
+      <SuportPokeball>{ComponetPokeball}</SuportPokeball>
+      <White></White>
+
+    </ContainerHeader>
+  )
+}
+
+export default Header
---
+++ silveira-pokedex-3/src/Components/Header/styles.jsx
@@ -0,0 +1,26 @@
+import styled from 'styled-components'
+
+export const ContainerHeader = styled.div`
+    display: flex;
+    flex-direction: column;
+    border: 1px solid black;
+`
+export const Red = styled.div`
+    height: 71px;
+    background-color: red;
+`
+export const Black = styled.div`
+    height: 10px;
+    background-color: black;
+`
+export const White = styled.div`
+    height: 71px;
+    background-color: white;
+`
+export const SuportPokeball = styled.div`
+    margin-top: 75px;
+    width: 100px;
+    height: 110px;
+    left: 100px;
+    position: absolute;
+`

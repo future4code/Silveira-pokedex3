d862c706c5b3cdce6edf4869952ffbd473d34b7c
Adeir Maia <adeir.maia@hotmail.com>
2022-05-04 16:27:02 -0300

    Corrido bug do myPokemos

--- silveira-pokedex-3/src/Components/CardPokemon/Card.js
+++ silveira-pokedex-3/src/Components/CardPokemon/Card.js
@@ -5,15 +5,14 @@ import { PokemonList } from '../GlobalStates/contexts'
 
 function Card(props) {
   const params = useContext(PokemonList)
-
   const arrayPokemons = params[`${props.batata}`] && params[`${props.batata}`].map(pokemon => {
     return (
       <ContainerCard key={pokemon.order}>
         <p>{pokemon.name.toUpperCase()}</p>
         <img src={pokemon.sprites.other.dream_world.front_default} />
         <div>
-          <Link to='/pokemon/pokedexdetails'><button onClick={() => params.getPokemonDetails(pokemon.id)}> Detalhes pokemon </button></Link>
-          <button onClick={() => params.addToMyPokemons(pokemon.id)}> Adicionar </button>
+          <Link to='/pokemon/pokedexdetails'><button onClick={() => params.getPokemonDetails(pokemon.id, props.place)}> Detalhes pokemon </button></Link>
+          <button onClick={() => params.addToMyPokemons(pokemon.id,)}> Adicionar </button>
         </div>
       </ContainerCard>
     )
